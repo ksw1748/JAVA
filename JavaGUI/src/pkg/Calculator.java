c79e53e51c6ccf29c30821f6957dc05d6aef1d7c
ksw1748 <ksw1748@gmail.com>
2024-10-20 21:03:25 +0900

    계산기로 오타수정

--- JavaGUI/src/pkg/Calculator.java
+++ JavaGUI/src/pkg/Calculator.java
@@ -1,10 +1,26 @@
 package pkg;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class Calculator extends JFrame {
 	Calculator() {
 		setTitle("게산기");
+		setLayout(new BorderLayout());
+		
+		
+		JPanel p = new JPanel();
+		p.setBackground(Color.WHITE);
+		
+		JTextArea t = new JTextArea(10, 38);
+		t.setBackground(Color.WHITE);
+		p.add(t);
+		add(p, BorderLayout.NORTH);
+		
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(400,550);
