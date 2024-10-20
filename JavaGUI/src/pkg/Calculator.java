499b6856a2759a426bf218fff42ffc3b00270d55
ksw1748 <ksw1748@gmail.com>
2024-10-20 22:14:10 +0900

    센터에 넣을 패널 추가

--- JavaGUI/src/pkg/Calculator.java
+++ JavaGUI/src/pkg/Calculator.java
@@ -2,6 +2,7 @@ package pkg;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,33 +10,29 @@ import javax.swing.JTextArea;
 
 public class Calculator extends JFrame {
 	Calculator() {
-		setTitle("게산기");
+		setTitle("계산기");
 		setLayout(new BorderLayout());
-		
-		
+
 		JPanel p = new JPanel();
 		p.setBackground(Color.WHITE);
-		
+
 		JTextArea t = new JTextArea(10, 38);
 		t.setBackground(Color.WHITE);
 		p.add(t);
 		add(p, BorderLayout.NORTH);
-		
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(400,550);
+		setSize(400, 550);
 		setVisible(true);
 		
-		
-		
-		
-		
+		JPanel p1 = new JPanel();
+		p1.setLayout(new GridLayout(5, 5, 2, 2)); 
+		add(p1, BorderLayout.CENTER);
+
 	}
 
 	public static void main(String[] args) {
 		new Calculator();
-		
-		
 
 	}
 
