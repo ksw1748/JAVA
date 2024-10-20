db4aef6480250db001247576b0596d37481f63b4
ksw1748 <ksw1748@gmail.com>
2024-10-20 22:27:12 +0900

    보더레이아웃 센터 패널 추가 버튼 이름 배열로 추가

--- JavaGUI/src/pkg/Calculator.java
+++ JavaGUI/src/pkg/Calculator.java
@@ -28,6 +28,9 @@ public class Calculator extends JFrame {
 		JPanel p1 = new JPanel();
 		p1.setLayout(new GridLayout(4, 4, 2, 2)); 
 		add(p1, BorderLayout.CENTER);
+		
+		String[] button_names = { "7", "8", "9", "x", "4", "5", "6", "/", "1", "2", "3","+", "C", "0", "=", "-" };
+
 
 	}
 
