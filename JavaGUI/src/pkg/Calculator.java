5e563b3f76ca158b0439d726ab2df700284a45d2
ksw1748 <ksw1748@gmail.com>
2024-10-20 22:51:07 +0900

    보더레이 아웃 센터부분 패널 추가

--- JavaGUI/src/pkg/Calculator.java
+++ JavaGUI/src/pkg/Calculator.java
@@ -29,7 +29,7 @@ public class Calculator extends JFrame {
 		p1.setLayout(new GridLayout(4, 4, 2, 2)); 
 		add(p1, BorderLayout.CENTER);
 		
-		String[] button_names = { "7", "8", "9", "x", "4", "5", "6", "/", "1", "2", "3","+", "C", "0", "=", "-" };
+		
 
 
 	}
