3540fc70db29cdf1021fbc88df1e0db9733d9ac2
ksw1748 <ksw1748@gmail.com>
2024-10-20 23:08:23 +0900

    버튼 생성 (반복문)

--- JavaGUI/src/pkg/Calculator.java
+++ JavaGUI/src/pkg/Calculator.java
@@ -4,9 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Calculator extends JFrame {
 	Calculator() {
@@ -16,21 +18,30 @@ public class Calculator extends JFrame {
 		JPanel p = new JPanel();
 		p.setBackground(Color.WHITE);
 
-		JTextArea t = new JTextArea(10, 38);
+		JTextField t = new JTextField(50);
 		t.setBackground(Color.WHITE);
 		p.add(t);
 		add(p, BorderLayout.NORTH);
 
+		JPanel p1 = new JPanel();
+		p1.setBackground(Color.WHITE);
+		p1.setLayout(new GridLayout(4, 4));
+		add(p1, BorderLayout.SOUTH);
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(400, 550);
 		setVisible(true);
-		
-		JPanel p1 = new JPanel();
-		p1.setLayout(new GridLayout(4, 4, 2, 2)); 
-		add(p1, BorderLayout.CENTER);
-		
-		
 
+		String[] button_names = { "7", "8", "9", "x", "4", "5", "6", "/", "1", "2", "3", "+", "C", "0", "=", "-" };
+
+		for (int i = 0; i < button_names.length; i++) {
+			JButton b = new JButton(button_names[i]);
+			b.setBackground(Color.GRAY);
+
+			p1.add(b);
+
+			
+		}
 
 	}
 
