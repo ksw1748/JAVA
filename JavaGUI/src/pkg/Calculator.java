46007627e08316fc391d8a16a401bcba937a664a
ksw1748 <ksw1748@gmail.com>
2024-10-20 23:20:23 +0900

    버튼 글씨 색상 변경

--- JavaGUI/src/pkg/Calculator.java
+++ JavaGUI/src/pkg/Calculator.java
@@ -17,26 +17,26 @@ public class Calculator extends JFrame {
 		JPanel p = new JPanel();
 		p.setBackground(Color.WHITE);
 
-		JTextArea t = new JTextArea(10,35);
+		JTextArea t = new JTextArea(10,40);
 		t.setBackground(Color.WHITE);
 		p.add(t);
 		add(p, BorderLayout.NORTH);
 
 		JPanel p1 = new JPanel();
 		p1.setBackground(Color.WHITE);
-		p1.setLayout(new GridLayout(4, 4));
-		add(p1, BorderLayout.SOUTH);
+		p1.setLayout(new GridLayout(4, 4, 2, 2));
+		add(p1, BorderLayout.CENTER);
 
 		String[] button_names = { "7", "8", "9", "x", "4", "5", "6", "/", "1", "2", "3", "+", "C", "0", "=", "-" };
 
 		for (int i = 0; i < button_names.length; i++) {
 			JButton b = new JButton(button_names[i]);
-			b.setBackground(Color.GRAY);
+			b.setBackground(Color.LIGHT_GRAY);
 
 			p1.add(b);
 
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			setSize(400, 550);
+			setSize(350, 550);
 			setVisible(true);
 
 		}
