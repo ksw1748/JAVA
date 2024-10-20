b7b5e7ba3fe77819b0f2ca4970ee4b0fe9d30fbd
ksw1748 <ksw1748@gmail.com>
2024-10-20 20:38:36 +0900

    계산기 틀 완성

--- JavaGUI/src/pkg/Calculator.java
+++ JavaGUI/src/pkg/Calculator.java
@@ -1,8 +1,24 @@
 package pkg;
 
-public class Calculator {
+import javax.swing.JFrame;
+
+public class Calculator extends JFrame {
+	Calculator() {
+		setTitle("게산기");
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400,550);
+		setVisible(true);
+		
+		
+		
+		
+		
+	}
 
 	public static void main(String[] args) {
+		new Calculator();
+		
 		
 
 	}
